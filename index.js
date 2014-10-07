859f036ac0e6bc26ee18b0bbc86a034ce543d801
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-07 11:07:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var mark = 'Search here',
-    edited = false;
+var mark = 'Search here';
+var edited = false;
+var selectedOption = 0;
+var numberOfOptions = 0;
 
 $('[contenteditable]').focus(function() {
     if(!edited) {
@@ -12,3 +14,69 @@ $('[contenteditable]').focus(function() {
 }).keyup(function() {
     edited = true;
 }).text(mark);
+
+
+$.expr[":"].containsIns = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
+
+function getRelevantOptions(words)
+{
+    var searchString = "";
+    
+    for(var i = 0; i < words.length; i++)
+    {
+        searchString = searchString + ".project-container:containsIns(" + words[i] + ") ";
+    }
+    
+    var relevantOptions = $(searchString);
+    
+	return relevantOptions;
+}
+
+function fullOptionString( relevantOptions )
+{
+    return relevantOptions;
+}
+
+function getOptions(e)
+{
+	if( e.which != 40 && e.which != 38 )
+	{
+		$("#search-results").removeClass("hidden");
+		
+		var typedText = $("#search-bar").text();
+		
+		var relevantWords =  typedText.split(' ');
+		
+		$("#search-results").html( fullOptionString( getRelevantOptions(relevantWords) ));
+	}
+}
+
+$('#search-bar').on( 'keydown', function( e ) {
+    if( e.which == 9 || e.which == 13) {
+    	e.preventDefault();
+    	
+        // Do Something on enter and tab
+        
+    }
+} )
+.keyup(function(e) {
+    if( e.which == 40 ){
+    	selectedOption = (selectedOption + 1) % numberOfOptions;
+    	$(".search-result").removeClass("selected");
+    	$("#sr" + selectedOption).addClass("selected");
+    }
+    else if( e.which == 38 ){
+    	selectedOption = (selectedOption + numberOfOptions - 1) % numberOfOptions;
+    	$(".search-result").removeClass("selected");
+    	$("#sr" + selectedOption).addClass("selected");
+    }
+    else{
+    	getOptions(e);
+    }
+    return false;
+});
